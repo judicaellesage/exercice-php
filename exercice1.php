2f2d7d6903e68a0063f147102086624434dbe40d
Judicael <judicojean@gmail.com>
2023-08-05 10:19:03 +0200

    create a multiplication table 1 to 10

---
+++ exercice1.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($i=0; $i <= 10; $i++) {
+    echo "Table de multiplication de ".$i."<br>";
+    for ($j=0; $j <= 10; $j++) {
+        echo $i." x ".$j." = ".$i*$j."<br>";
+    }
+    echo "<br>";
+}
